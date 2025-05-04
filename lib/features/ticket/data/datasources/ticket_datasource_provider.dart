74e41906000b22ab4c79dad0960fd82e3a19c8bc
Adrian <adrian_ferreiro@hotmail.com>
2025-05-04 15:28:29 -0300

    feat(Ticket): firebase injection
    PR: #1

--- lib/features/ticket/data/datasources/ticket_datasource_provider.dart
+++ lib/features/ticket/data/datasources/ticket_datasource_provider.dart
@@ -1,11 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tickify_flutter/features/ticket/data/datasources/ticket_datasource.dart';
 import 'package:tickify_flutter/features/ticket/data/datasources/firebase_datasource_impl.dart';
+
+final firebaseFirestoreProvider = Provider<FirebaseFirestore>((ref) {
+  return FirebaseFirestore.instance;
+});
 
 ///
 /// [ticketDatasourceProvider] expose datasource
 ///
 final ticketDatasourceProvider = Provider<TicketDatasource>((ref) {
-  final ticketDatasourceImpl = FirebaseDatasourceImpl();
-  return ticketDatasourceImpl;
+  final firestore = ref.watch(firebaseFirestoreProvider);
+  return FirebaseDatasourceImpl(firestore);
 });
