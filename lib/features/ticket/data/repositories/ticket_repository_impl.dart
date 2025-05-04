230eb0e7d5fd494024fb35eb521de89807f773d8
Adrian <adrian_ferreiro@hotmail.com>
2025-05-04 14:15:59 -0300

    feat(Tikect): repository y datasource
    PR: #1

--- lib/features/ticket/data/repositories/ticket_repository_impl.dart
+++ lib/features/ticket/data/repositories/ticket_repository_impl.dart
@@ -1,21 +1,22 @@
 import 'package:dartz/dartz.dart';
 import 'package:tickify_flutter/core/network/errors/failures.dart';
 import 'package:tickify_flutter/features/ticket/data/datasources/ticket_datasource.dart';
+import 'package:tickify_flutter/features/ticket/data/mappers/ticket_mapper.dart';
 import 'package:tickify_flutter/features/ticket/domain/entities/ticket_entity.dart';
 import 'package:tickify_flutter/features/ticket/domain/repositories/ticket_repository.dart';
 
 class TicketRepositoryImpl implements TicketRepository {
   final TicketDatasource dataSource;
 
-  TicketRepositoryImpl(this.dataSource);
+  TicketRepositoryImpl({required this.dataSource});
 
   @override
   Future<Either<Failure, TicketEntity>> validateTicket(String ticketId) async {
     try {
       final response = await dataSource.validateTicket(ticketId);
-      return Right(response);
+      return Right(response.toEntity());
     } catch (_) {
-      return Left(ApiFailure);
+      return Left(ApiFailure());
     }
   }
 }
--- lib/features/ticket/domain/repositories/ticket_repository_provider.dart
+++ lib/features/ticket/domain/repositories/ticket_repository_provider.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tickify_flutter/features/ticket/data/datasources/ticket_datasource_provider.dart';
+import 'package:tickify_flutter/features/ticket/data/repositories/ticket_repository_impl.dart';
 import 'package:tickify_flutter/features/ticket/domain/repositories/ticket_repository.dart';
 
 ///
 /// [ticketRepositoryProvider] expose repository with [TicketDatasource] injected
 ///
 final ticketRepositoryProvider = Provider<TicketRepository>((ref) {
-  final dataSource = ref.read(pokedexDatasourceProvider);
-  final pokedexRepositoryImpl = PokedexRepositoryImpl(dataSource: dataSource);
+  final dataSource = ref.read(ticketDatasourceProvider);
+  final pokedexRepositoryImpl = TicketRepositoryImpl(dataSource: dataSource);
 
   return pokedexRepositoryImpl;
 });
